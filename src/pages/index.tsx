4398edab18d7c531047c6da8aea0d24b21e1818f
dfilastro <d.filastro@hotmail.com>
2022-04-09 02:24:45 -0300

    posts rendered from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,19 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Prismic from '@prismicio/client';
 
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { createClient } from '../services/prismic';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -29,7 +35,19 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ posts }) {
+  // const formatedPosts = postsPagination.results.map(post => {
+  //   return {
+  //     ...post,
+  //     first_publication_date: format(
+  //       new Date(post.first_publication_date),
+  //       'dd MMM yyyy',
+  //       { locale: ptBR }
+  //     ),
+  //   };
+  // });
+
+  // const [posts, setPosts] = useState<Post[]>(formatedPosts);
   return (
     <>
       <Head>
@@ -38,94 +56,26 @@ export default function Home() {
 
       <main className={commonStyles.container}>
         <Header />
+
         <section className={styles.container}>
-          <div className={styles.postContainer}>
-            <h1 className={styles.postTitle}>Como Utilizar Hooks</h1>
-            <p className={styles.postAbstract}>
-              Pensando em sincronização em vez de ciclos de vida
-            </p>
-            <div className={styles.postData}>
-              <div>
-                <FiCalendar />
-                <p>22 Mar 2022</p>
+          {posts?.map(post => (
+            <Link href={`/posts/${post.uid}`} key={post.uid}>
+              <div className={styles.postContainer}>
+                <h1 className={styles.postTitle}>{post.data.title}</h1>
+                <p className={styles.postAbstract}>{post.data.subtitle}</p>
+                <div className={styles.postData}>
+                  <div>
+                    <FiCalendar />
+                    <p>{post.first_publication_date}</p>
+                  </div>
+                  <div>
+                    <FiUser />
+                    <p>{post.data.author}</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <FiUser />
-                <p>Rikieri Sartor</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.postContainer}>
-            <h1 className={styles.postTitle}>Criando um App CRA do zero</h1>
-            <p className={styles.postAbstract}>
-              Tudo sobre como criar sua primeira aplicação usando o Create React
-              App
-            </p>
-            <div className={styles.postData}>
-              <div>
-                <FiCalendar />
-                <p>19 Abr 2022</p>
-              </div>
-              <div>
-                <FiUser />
-                <p>Diego Filastro</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.postContainer}>
-            <h1 className={styles.postTitle}>Como Utilizar Hooks</h1>
-            <p className={styles.postAbstract}>
-              Pensando em sincronização em vez de ciclos de vida
-            </p>
-            <div className={styles.postData}>
-              <div>
-                <FiCalendar />
-                <p>22 Mar 2022</p>
-              </div>
-              <div>
-                <FiUser />
-                <p>Rikieri Sartor</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.postContainer}>
-            <h1 className={styles.postTitle}>Criando um App CRA do zero</h1>
-            <p className={styles.postAbstract}>
-              Tudo sobre como criar sua primeira aplicação usando o Create React
-              App
-            </p>
-            <div className={styles.postData}>
-              <div>
-                <FiCalendar />
-                <p>19 Abr 2022</p>
-              </div>
-              <div>
-                <FiUser />
-                <p>Diego Filastro</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.postContainer}>
-            <h1 className={styles.postTitle}>Criando um App CRA do zero</h1>
-            <p className={styles.postAbstract}>
-              Tudo sobre como criar sua primeira aplicação usando o Create React
-              App
-            </p>
-            <div className={styles.postData}>
-              <div>
-                <FiCalendar />
-                <p>19 Abr 2022</p>
-              </div>
-              <div>
-                <FiUser />
-                <p>Diego Filastro</p>
-              </div>
-            </div>
-          </div>
+            </Link>
+          ))}
 
           <button type="button">Carregar mais posts</button>
         </section>
@@ -134,9 +84,31 @@ export default function Home() {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const client = createClient({ previewData });
 
-//   // TODO
-// };
+  const response = await client.getAllByType('posts', {
+    pageSize: 1,
+  });
+
+  const posts = response.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title[0].text,
+        subtitle: post.data.subtitle[0].text,
+        author: post.data.author[0].text,
+      },
+    };
+  });
+
+  // const postsPagination = {
+  //   next_page: response.next_page,
+  //   results: posts,
+  // };
+
+  return {
+    props: { posts },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,43 @@
-import Prismic from '@prismicio/client';
-import { DefaultClient } from '@prismicio/client/types/client';
+import * as prismic from '@prismicio/client';
+import { enableAutoPreviews } from '@prismicio/next';
+import PreviewData, { NextApiRequest } from 'next';
+// import sm from '../../sm.json';
 
-export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    req,
+interface PrismicContext {
+  req?: NextApiRequest;
+  previewData: any;
+}
+
+// todo need refactory
+interface PrismicResolver {
+  [key: string]: any;
+}
+
+export const endpoint = process.env.PRISMIC_API_EDNPOINT;
+export const repositoryName = prismic.getRepositoryName(endpoint);
+
+// Update the Link Resolver to match your project's route structure
+export function linkResolver(doc: PrismicResolver) {
+  switch (doc.type) {
+    case 'projects':
+      return `/${doc.uid}`;
+    default:
+      return null;
+  }
+}
+
+// This factory function allows smooth preview setup
+export function createClient(config: PrismicContext) {
+  const client = prismic.createClient(endpoint, {
+    ...config,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
 
-  return prismic;
+  enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req,
+  });
+
+  return client;
 }
